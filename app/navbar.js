c2fbb224da09434a4b2c8bfe3ce418a68a17c8ee
Roberto Vieira Rezende <robertovrd2106@gmail.com>
2023-05-01 11:20:05 -0300

    navbar att 2

--- app/navbar.js
+++ app/navbar.js
@@ -33,13 +33,13 @@ export default function Navbar() {
           <a href="/" className={`${MontTitle.className} block py-2 pl-3 pr-4 text-white bg-white rounded md:bg-transparent md:text-white md:p-0 dark:text-white`} aria-current="page">Inicio</a>
         </li>
         <li>
-          <a href="/consultoria" className={`${MontTitle.className} block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Serviços</a>
+          <a href="/" className={`${MontTitle.className} block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Serviços</a>
         </li>
         <li>
-          <a href="/desenvolvimento" className={`${MontTitle.className} block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Projetos</a>
+          <a href="/" className={`${MontTitle.className} block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Projetos</a>
         </li>
         <li>
-          <a href="/analise" className={`${MontTitle.className} block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Sobre</a>
+          <a href="/" className={`${MontTitle.className} block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Sobre</a>
         </li>
       </ul>
     </div>
