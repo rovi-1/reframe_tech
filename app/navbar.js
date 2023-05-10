117c84bfe8c82dbb5263c68e51306c851555b3b2
Roberto Vieira Rezende <robertovrd2106@gmail.com>
2023-05-09 21:26:50 -0300

    agora vai

--- app/navbar.js
+++ app/navbar.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import styles from './page.module.css';
 import { Orbitron } from 'next/font/google';
 import { Montserrat } from 'next/font/google';
-import { NavLink } from "react-router-dom";
+
 
 export const Header = () => {
   const router = useRouter();
@@ -35,16 +35,16 @@ export default function Navbar() {
           <div class="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
         <li>
-          <NavLink  to={"/"} className={`${MontTitle.className} [&.active]:text-white block py-2 pl-3 pr-4 text-gray-700 bg-white rounded md:bg-transparent md:text-white md:p-0 dark:text-white`} aria-current="page">Inicio</NavLink>
+          <a  href='/' className={`${MontTitle.className} [&.active]:text-white block py-2 pl-3 pr-4 text-gray-700 bg-white rounded md:bg-transparent md:text-white md:p-0 dark:text-white`} aria-current="page">Inicio</a>
         </li>
         <li>
-          <NavLink  to={"/"} className={`${MontTitle.className} [&.active]:text-white-500 block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Serviços</NavLink>
+          <a  href='/' className={`${MontTitle.className} [&.active]:text-white-500 block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Serviços</a>
         </li>
         <li>
-          <NavLink  to={"/"} className={`${MontTitle.className}  [&.active]:text-white-500 block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Projetos</NavLink>
+          <a  href='/' className={`${MontTitle.className}  [&.active]:text-white-500 block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Projetos</a>
         </li>
         <li>
-          <NavLink  to={"/"} className={`${MontTitle.className}  [&.active]:text-white-500 block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Sobre</NavLink>
+          <a  href='/' className={`${MontTitle.className}  [&.active]:text-white-500 block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Sobre</a>
         </li>
       </ul>
     </div>
